af3b88af46382d8278ef1f2115bff0017fbad230
JayPolanco <44456195+JayPolanco@users.noreply.github.com>
2018-12-19 18:23:31 -0500

    Add files via upload

---
+++ CurrentVersion/admin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+	
+		<div class="mainSection">
+			
+			
+			<div class="crawlContainer">
+			
+				<form action="crawl.php" method="GET">
+				
+					<input class="searchBox" type="text" name="term" placeholder="Enter website">
+					<input class="searchButton" type="submit" value="Crawl">
+				
+				</form>
+			
+			</div>
+			
+			<div class="databaseContainer">
+				
+			</div>
+		
+		</div>
+		
+	</div>
+
+	
+</body>
+</html>
